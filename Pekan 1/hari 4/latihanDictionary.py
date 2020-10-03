fa78b9ddafdbf28d83bf90eb4ee0953515b5293a
unknown <ariwilyan@gmail.com>
2020-10-03 19:44:12 +0700

    update tugas 4 dan 5 pekan 1

--- Pekan 1/hari 4/latihanDictionary.py
+++ Pekan 1/hari 4/latihanDictionary.py
@@ -27,3 +27,15 @@ member2 = {"ID": 102,
 memberlist = {101: member1, 102: member2}
 
 print(memberlist[101])
+
+#a = [4, 7, 2, 6, 13, 5, 3, 0, 1]
+#a.sort() # jika mau menggunakan method sort
+#print(sorted(a))
+import random
+nama = 'Ari Wilyan'
+jawaban_nama = [
+	"nama saya  {0}".format(nama),
+	"orang-orang memanggil saya {0}".format(nama),
+	"panggil saja saya {0}".format(nama)
+]
+print(random.choice(jawaban_nama))
